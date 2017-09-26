cdf957f288114243aae485308a60c658c6d98f2c
KorJuk69 <korjuk1@gmail.com>
2017-09-26 14:26:35 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,25 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        double a = scanner.nextInt();
+        double belki = scanner.nextDouble();
+        double jiri = scanner.nextDouble();
+        double vuglev = scanner.nextDouble();
+        int n = scanner.nextInt();
+        int no = 0;
+
+        for(int i=0;i<n;i++){
+
+            double nbelki = scanner.nextDouble();
+            double njiri = scanner.nextDouble();
+            double nvuglev = scanner.nextDouble();
+            double kolvo = scanner.nextDouble();
+
+            boolean nope = (nbelki*kolvo!=belki)&&(njiri*kolvo!=jiri)&&(nvuglev*kolvo!=vuglev);
+            if (nope) no = 1;
+
+        }
+
+        System.out.print(no == 1 ? "NO" : "Yes");
 
     }
 }
