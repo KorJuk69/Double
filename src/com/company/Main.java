40348a9b892c22cbcf052a9bf139b0d55370ca1b
KorJuk69 <korjuk1@gmail.com>
2017-09-26 14:04:57 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,12 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        double a = scanner.nextInt();
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        if (a+b==c) System.out.print("Yes");
+        else System.out.print("NO");
 
     }
 }
